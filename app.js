8000c3b8a487f5d76fa50706609bd364d4363e03
shazi <138199527@qq.com>
2018-06-28 20:28:20 +0800

    配置静态资源和模板引擎

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = require('./routes/router')
+const art = require('express-art-template')
 const app = express()
+
+app.use('/public', express.static('./public'))
+app.use('/node_modules', express.static('./node_modules'))
+app.engine('html', art)
 
 const POST = 3000
 app.listen(POST, () => console.log('success'))
